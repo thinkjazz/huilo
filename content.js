fa762a17a52d3e1ebc8928c5fef25ad9cb1b17fd
Bragin Boris <bragin.boris@ya.ru>
2024-05-30 12:29:25 +0300

    Add new replacment pairs

--- content.js
+++ content.js
@@ -1,22 +1,36 @@
 const replacements = {
-    'Путина': 'Хуйла',
-    'путина': 'хуйла',
-    'Путин': 'Хуйло',
-    'путин': 'хуйло',
-    'Путиным': 'Хуйлом',
-    'путиным': 'хуйлом',
-    'Пу́тин': 'Ху́йло',
-    'Putin': 'Huilo',
-    'putin': 'huilo',
+  'Путина': 'Хуйла',
+  'путина': 'хуйла',
+  'Путине': 'Хуйле',
+  'путине': 'хуйле',
+  'Путину': 'Хуйлу',
+  'путину': 'хуйлу',
+  'Путин': 'Хуйло',
+  'путин': 'хуйло',
+  'Путиным': 'Хуйлом',
+  'путиным': 'хуйлом',
+  'Пу́тин': 'Ху́йло',
+  'Putin': 'Huilo',
+  'putin': 'huilo',
+  'Путін': 'Хуйло',
+  'путін': 'Хуйло',
+  'Путіна': 'Хуйла',
+  'путіна': 'хуйла',
+  'Путіне': 'Хуйле',
+  'путіне': 'хуйле',
+  'Путіну': 'Хуйлу',
+  'путіну': 'хуйлу',
+  'Путіным': 'Хуйлом',
+  'путіным': 'хуйлом',
+};
 
-  };
-  
+
   const regex = new RegExp(Object.keys(replacements).join("|"), "gi");
-  
+
   function replaceTextContent(textContent) {
     return textContent.replace(regex, (matched) => replacements[matched]);
   }
-  
+
   function traverseAndReplace(node) {
     const walker = document.createTreeWalker(node, NodeFilter.SHOW_TEXT, null, false);
     let textNode;
@@ -24,15 +38,14 @@ const replacements = {
       textNode.textContent = replaceTextContent(textNode.textContent);
     }
   }
-  
+
   function replaceTextInDocument() {
     traverseAndReplace(document.body);
   }
-  
+
   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "replaceText") {
       replaceTextInDocument();
       sendResponse({ status: "done" });
     }
   });
-  
